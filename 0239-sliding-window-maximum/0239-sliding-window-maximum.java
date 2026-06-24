ff92ba0c1dcd81675fe7503429cd4643e1a111aa
jagmohan83 <rajkumarverma3266@gmail.com>
2026-06-24 13:44:39 +0530

    Time: 9 ms (99.2%), Space: 148.4 MB (43.49%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -3,20 +3,30 @@ class Solution {
         int n = nums.length;
         int[] result = new int[n-k+1];
         int ri =0;
-        Deque<Integer> q = new ArrayDeque<>();
-        for(int i = 0; i<n; i++){
-            if(!q.isEmpty() && q.peekFirst() <i-k+1){
-                q.pollFirst();
+        int[] customQueue = new int[n];
+        int head = 0; 
+        int tail = 0; 
+        
+        for (int i = 0; i < n; i++) {
+           
+            if (head < tail && customQueue[head] < i - k + 1) {
+                head++; 
             }
-            while(!q.isEmpty() && nums[q.peekLast()] <nums[i]){
-                q.pollLast();
+            
+           
+            while (head < tail && nums[customQueue[tail - 1]] < nums[i]) {
+                tail--; 
             }
-            q.offerLast(i);
-            if(i>=k-1){
-                result[ri++]= nums[q.peekFirst()];
+            
+           
+            customQueue[tail++] = i;
+            
+            
+            if (i >= k - 1) {
+                result[ri++] = nums[customQueue[head]];
             }
-
         }
+        
         return result;
     }
 }
