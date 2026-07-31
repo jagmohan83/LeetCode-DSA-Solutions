f4a9bebf6f18789945dfef8245be34a858a43181
jagmohan83 <rajkumarverma3266@gmail.com>
2026-07-31 13:15:41 +0530

    Time: 131 ms (67.48%), Space: 43.1 MB (44.34%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        boolean[][] visited = new boolean[board.length][board[0].length];
+      
         for(int i = 0; i<board.length; i++){
             for(int j = 0; j<board[0].length; j++){
                 if(board[i][j]==word.charAt(0)){
-                    if(dfs(0,board,visited,word, i, j)){
+                    if(dfs(0,board,word, i, j)){
                         return true;
                     }
                 }
@@ -12,29 +12,29 @@ class Solution {
         }
         return false;
     }
-    private boolean dfs(int index, char[][] board, boolean[][] visited, String word, int i, int j){
+    private boolean dfs(int index, char[][] board, String word, int i, int j){
          if(index==word.length()){
             return true;
         }
-        if(i<0 || j<0 || i>=board.length || j>=board[0].length){
+        if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j] != word.charAt(index)){
             return false;
         }
        
-        if(board[i][j] != word.charAt(index)){
-            return false;
-        }
-        if(visited[i][j]){
-            return false;
-        }
-        visited[i][j]= true;
-        boolean right = dfs(index+1,board,visited,word,i,j+1);
-        boolean down = dfs(index+1,board,visited,word,i+1,j);
-        boolean left = dfs(index+1,board,visited,word,i,j-1);
-        boolean up = dfs(index+1,board,visited,word,i-1,j);
-        boolean ans = right || down || left || up;
-        visited[i][j]=false;
-        return ans;
-    }
+        char temp = board[i][j];
+        
+       
+        board[i][j] = '#';
 
         
+        boolean found = dfs(index + 1, board, word, i, j + 1) ||
+                        dfs(index + 1, board, word, i + 1, j) ||
+                        dfs(index + 1, board, word, i, j - 1) ||
+                        dfs(index + 1, board, word, i - 1, j);
+
+      
+        board[i][j] = temp;
+
+        return found;
+    }
 }
+    
